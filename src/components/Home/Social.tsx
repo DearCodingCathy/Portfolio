341baa8135ae3e6f2c2175339fd2eb6f3111fc70
Catherine <balogun.catherine@yahoo.com>
2023-11-20 16:59:02 -0500

    Clear compiled warnings

--- src/components/Home/Social.tsx
+++ src/components/Home/Social.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Social = () => {
   return (
       <div className="home__social">
-      <a href="https://instagram.com/codingcathy" className="home__social-icon" target="_blank">
+      <a href="https://instagram.com/codingcathy" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-instagram"></i>
       </a>
 
-      <a href="" className="home__social-icon" target="_blank">
+      {/* <a href="" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-dribbble"></i>
-      </a>
+      </a> */}
 
-      <a href="https://github.com/DearCodingCathy" className="home__social-icon" target="_blank">
+      <a href="https://github.com/DearCodingCathy" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-github-alt"></i>
       </a>
 
-      <a href="https://www.linkedin.com/in/catherinebalogun" className="home__social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/catherinebalogun" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-linkedin-alt"></i>
       </a>
     </div>
