616729b84080a0c0db90f78b1cd4d1e1c11310f4
Catherine <balogun.catherine@yahoo.com>
2023-11-29 15:21:35 -0500

    Add Testimonials

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About/About.tsx";
 import Skills from "./components/Skills/Skills";
 import Services from "./components/Services/Services";
 import Qualifications from "./components/Qualifications/Qualifications";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Services />
         <Qualifications />
+        <Testimonials />
       </main>
     </>
   );
